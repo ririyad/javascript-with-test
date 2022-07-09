551c1216530c6d8bef6eefb619f491621fd9cd92
Rashedul Islam Riyad <rasheduliriyad@gmail.com>
2022-07-09 18:48:34 +0600

    Removed spaces

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,6 @@
 const hello = require('../src/index.js');
 const assert = require('assert').strict;
 
-
 describe('First test', () => {
     it('hello universe', ()=> { 
         assert.equal(hello(), 'Hello, Universe!');
